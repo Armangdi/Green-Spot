5274241d190710cae584d7c970f4b53e11d1ff86
Jaemin You <108450957+Armangdi@users.noreply.github.com>
2022-11-16 00:43:14 +0900

    Add files via upload

---
+++ src/main/java/edu/skku/cs/mapreview/MapActivity.java
@@ -0,0 +1,146 @@
+package edu.skku.cs.mapreview;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class MapActivity extends AppCompatActivity implements MySimpleContract.ContractForView, OnMapReadyCallback{
+    private View decorView;
+    private int	uiOption;
+    private MySimplePresenter presenter;
+    private MapView mapView;
+    private NaverMap naverMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private Button btn_current, btn_view, btn_list;
+    private int dark;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
+            uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+            uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOption);
+
+        mapView = findViewById(R.id.map_view);
+        presenter = new MySimplePresenter(this, new MySimpleModel(presenter, MapActivity.this));
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        mapView.onCreate(savedInstanceState);
+
+        btn_current = findViewById(R.id.btn_current);
+        btn_view = findViewById(R.id.btn_view);
+        btn_list = findViewById(R.id.btn_list);
+        presenter.setMarkers(naverMap);
+        btn_current.setOnClickListener(view -> {
+            presenter.onCurrentClicked(naverMap);
+        });
+
+        btn_view.setOnClickListener(view -> {
+            presenter.onViewClicked(naverMap);
+        });
+        btn_list.setOnClickListener(view -> {
+            presenter.onListClicked(naverMap);
+        });
+
+        mapView.getMapAsync(this);
+    }
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults
+        );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+    @Override
+    public void onLoginResult(Boolean result) {
+        //btn.setText("Value: " + value); // update UI\
+    }
+
+    @Override
+    public void displayValue(int value) {
+        //btn.setText("Value: " + value); // update UI
+    }
+
+    @Override
+    public void setMarker(Marker marker, double lat, double lon, int resourceID){
+        marker.setIconPerspectiveEnabled(true);
+        marker.setIcon(OverlayImage.fromResource(resourceID));
+        marker.setAlpha(0.8f);
+        marker.setPosition(new LatLng(lat, lon));
+        marker.setMap(naverMap);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        //ActivityCompat.requestPermissions(this, PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+    }
+}
---
+++ src/main/java/edu/skku/cs/mapreview/MySimplePresenter.java
@@ -0,0 +1,338 @@
+package edu.skku.cs.mapreview;
+
+import static java.lang.Math.round;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraAnimation;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MySimplePresenter implements MySimpleContract.ContractForPresenter{
+
+    public ArrayList<Double> lats = new ArrayList<Double>();
+    public ArrayList<Double> lons = new ArrayList<Double>();
+    public ArrayList<String> names = new ArrayList<String>();
+    private MySimpleContract.ContractForView view;
+    private MySimpleContract.ContractForModel model;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private double near_lat, near_lon;
+    private int map_mode = 0;
+    private int dark = 0;
+    private ArrayList<Marker> markers = new ArrayList<Marker>();
+
+    public MySimplePresenter(MySimpleContract.ContractForView view,
+                             MySimpleContract.ContractForModel model){
+        this.view = view;
+        this.model = model;
+    }
+
+
+    @Override
+    public void onCurrentClicked(NaverMap naverMap){
+        double[] lo;
+
+        lo = model.doCurrent();
+
+        CameraUpdate cameraUpdate = CameraUpdate.scrollAndZoomTo(new LatLng(lo[0], lo[1]),15).animate(CameraAnimation.Fly, 2000);;
+        naverMap.moveCamera(cameraUpdate);
+    }
+
+
+
+    @Override
+    public void setMarkers(NaverMap naverMap){
+        OkHttpClient client = new OkHttpClient();
+
+        /*
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("DB @@@@").newBuilder();
+        urlBuilder.addQueryParameter("Key", "@@@@@@@@");
+        urlBuilder.addQueryParameter("Type", "Json");
+        String url = urlBuilder.build().toString();
+
+        Request req = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String myResponse = response.body().string();
+                JSONObject jObject = null;
+                try {
+                    jObject = new JSONObject(myResponse);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                JSONArray jArray = null;
+                JSONObject jArray2 = null;
+                JSONArray jArray3 = null;
+
+                try {
+                    jArray = jObject.getJSONArray("");
+                    jArray2 = jArray.getJSONObject(1);
+                    jArray3 = jArray2.getJSONArray("row");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                final JSONArray jArray4 = jArray3;
+
+                for (int i = 0; i < jArray4.length(); i++) {
+                    JSONObject obj = null;
+                    try {
+                        obj = jArray4.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String lat="", lon="", Center_name="";
+                    try {
+                        Center_name = obj.getString("CENTER_NM");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        lat = obj.getString("LAT");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        lon = obj.getString("LOGT");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Double d_lat = Double.parseDouble(lat);
+                    Double d_lon = Double.parseDouble(lon);
+                    lats.add(d_lat);
+                    lons.add(d_lon);
+                    names.add(Center_name);
+
+                }
+
+            }
+
+        });
+
+         */
+    }
+
+    @Override
+    public void onListClicked(NaverMap naverMap){
+        Intent intent = new Intent(model.getActivity(), ListActivity.class);
+        model.getActivity().startActivity(intent);
+    }
+
+    @Override
+    public void onViewClicked(NaverMap naverMap){
+        map_mode = (map_mode + 1) % 5;
+        switch(map_mode){
+            case 0:
+                naverMap.setMapType(NaverMap.MapType.Basic);
+                break;
+            case 1:
+                naverMap.setMapType(NaverMap.MapType.Navi);
+                break;
+            case 2:
+                naverMap.setMapType(NaverMap.MapType.Satellite);
+                break;
+            case 3:
+                naverMap.setMapType(NaverMap.MapType.Hybrid);
+                break;
+            case 4:
+                naverMap.setMapType(NaverMap.MapType.Terrain);
+                break;
+        }
+    }
+
+    @Override
+    public void onSentiClicked(String content) {
+        OkHttpClient client = new OkHttpClient();
+        DataModel dataModel = new DataModel();
+
+        Gson gson = new Gson();
+        dataModel.setContent(content);
+
+        String json = gson.toJson(dataModel, DataModel.class);
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze").newBuilder();
+        String url = urlBuilder.build().toString();
+
+        Request req = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(MediaType.parse("application/json"),json))
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String myResponse = response.body().string();
+                Gson gson = new GsonBuilder().create();
+                final DataModel data1 = gson.fromJson(myResponse,  DataModel.class);
+                model.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(data1.getSuccess()){
+                            Toast.makeText(model.getActivity(),"Login Success!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(model.getActivity(), MapActivity.class);
+                            model.getActivity().startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(model.getActivity(),"Login Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+
+        });
+
+
+    }
+
+    @Override
+    public void onLoginClicked(String id, String pw) {
+        OkHttpClient client = new OkHttpClient();
+        DataModel dataModel = new DataModel();
+
+        Gson gson = new Gson();
+        dataModel.setName(id);
+        dataModel.setPasswd(pw);
+
+        Intent intent = new Intent(model.getActivity(), MapActivity.class);
+        model.getActivity().startActivity(intent);
+        /*
+        String json = gson.toJson(dataModel, DataModel.class);
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("login url").newBuilder();
+        String url = urlBuilder.build().toString();
+
+        Request req = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(MediaType.parse("application/json"),json))
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String myResponse = response.body().string();
+                Gson gson = new GsonBuilder().create();
+                final DataModel data1 = gson.fromJson(myResponse,  DataModel.class);
+                model.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(data1.getSuccess()){
+                            Toast.makeText(model.getActivity(),"Login Success!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(model.getActivity(), MapActivity.class);
+                            model.getActivity().startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(model.getActivity(),"Login Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+
+        });
+*/
+
+    }
+
+    @Override
+    public void onSignClicked(){
+        Intent intent = new Intent(model.getActivity(), RegisterActivity.class);
+        model.getActivity().startActivity(intent);
+    }
+
+    @Override
+    public void onSignUpClicked(String id, String pw){
+        OkHttpClient client = new OkHttpClient();
+        DataModel dataModel = new DataModel();
+
+        Gson gson = new Gson();
+        dataModel.setName(id);
+        dataModel.setPasswd(pw);
+
+        String json = gson.toJson(dataModel, DataModel.class);
+        //add user API
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("adduser").newBuilder();
+        String url = urlBuilder.build().toString();
+
+        Request req = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(MediaType.parse("application/json"),json))
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String myResponse = response.body().string();
+                Gson gson = new GsonBuilder().create();
+                final DataModel data1 = gson.fromJson(myResponse,  DataModel.class);
+                model.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(data1.getSuccess()){
+                            Toast.makeText(model.getActivity(),"Register Success!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(model.getActivity(), LoginActivity.class);
+                            model.getActivity().startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(model.getActivity(),"Register Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+
+        });
+    }
+
+}
